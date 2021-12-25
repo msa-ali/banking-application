bf9c9ef89f18e5739a407084f5ea49b1a0bdb6ab
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2021-12-25 20:18:27 +0530

    using Custom Multiplexer

--- app/app.go
+++ app/app.go
@@ -6,10 +6,13 @@ import (
 )
 
 func Start() {
+
+	mux := http.NewServeMux()
+
 	// define routes
-	http.HandleFunc("/greet", Greet)
-	http.HandleFunc("/customers", GetAllCustomers)
+	mux.HandleFunc("/greet", Greet)
+	mux.HandleFunc("/customers", GetAllCustomers)
 
 	// starting server
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	log.Fatal(http.ListenAndServe("localhost:8000", mux))
 }
